d10b22be14f02992e0620786c445ffba51b4ba14
George  Pianka <gpianka1@gmail.com>
2019-02-17 13:46:34 +0000

    Automatically backed up by Learn

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -2,7 +2,7 @@ class SecretsController < ApplicationController
   
   def show
     if !!current_user
-      render '/secrets/show'
+      render :show
     else
       redirect_to root_path
     end
